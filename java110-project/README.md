d6edf90e7395c5abe5913e8f5135f6671b47d9ee
eomjinyoung <jinyoung.eom@gmail.com>
2018-09-04 14:37:46 +0900

    배열을 이용하여 여러 회원의 정보 저장

--- java110-project/README.md
+++ java110-project/README.md
@@ -2,12 +2,29 @@
 
 수업 관리 시스템 만들기
 
+## v0.2
+
+- 배열을 이용하여 여러 회원의 정보를 저장하기
+
 ## v0.1
 
-- 회원 정보 입력 받아 출력하기
+- 반복문을 이용하여 여러 회원의 정보를 입력 받아 출력하기
 - 작업
     - App.java 변경
 
 ## Initial Commit 
 
 - 자바 프로젝트 디렉토리 구성하기
+
+```
+1) 자바 프로젝트 디렉토리 구성하기
+$ gradle init --type java-application
+
+2) Gradle 설정 파일에 'eclipse' 플러그인 추가
+build.gradle 파일에 id 'eclipse' 추가
+
+3) 이클립스 설정 파일 생성
+$ gradle eclipse
+
+4) 이클립스에서 프로젝트를 임포트
+```
--- java110-project/src/main/java/App.java
+++ java110-project/src/main/java/App.java
@@ -2,22 +2,26 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+        String[] names = new String[100];
+        String[] emails = new String[100];
+        String[] passwords = new String[100];
         
-        // 1) 키보드 입력을 처리할 객체 준비
+        int index = 0;
+        
         Scanner keyIn = new Scanner(System.in);
         
-        // 2) 사용자로부터 회원 정보 입력 받기
         
         while (true) {
             System.out.print("이름? ");
-            String name = keyIn.nextLine();
+            names[index] = keyIn.nextLine();
             
             System.out.print("이메일? ");
-            String email = keyIn.nextLine();
+            emails[index] = keyIn.nextLine();
             
             System.out.print("암호? ");
-            String password = keyIn.nextLine();
-            System.out.printf("%s, %s, %s\n", name, email, password);
+            passwords[index] = keyIn.nextLine();
+            
+            index++;
             
             System.out.print("계속 하시겠습니까?(Y/n) ");
             String answer = keyIn.nextLine();
@@ -25,7 +29,12 @@ public class App {
                 break;
         }
         
+        for (int i = 0; i < index; i++) {
+            System.out.printf("%s, %s, %s\n", 
+                    names[i], emails[i], passwords[i]);
+        }
         
+        keyIn.close();
     }
 }
 
