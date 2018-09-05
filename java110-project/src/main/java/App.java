1103fcb5879917a77f2506db27bf5a4c4dd1b2aa
eomjinyoung <jinyoung.eom@gmail.com>
2018-09-05 10:17:49 +0900

    메뉴기능 추가

--- java110-project/src/main/java/App.java
+++ java110-project/src/main/java/App.java
@@ -38,11 +38,58 @@ public class App {
 
     public static void main(String[] args) {
         
-        inputMembers();
-        
-        printMembers();
+        while (true) {
+            String menu = promptMenu();
+            
+            if (menu.equals("1")) {
+                serviceStudentMenu();
+            } else if (menu.equals("0")){
+                System.out.println("안녕히 가세요!");
+                break;
+            }
+        }
         
         keyIn.close();
+    }
+
+    private static void serviceStudentMenu() {
+        while (true) {
+            System.out.print("학생 관리> ");
+            String command = keyIn.nextLine();
+            if (command.equals("list")) {
+                printMembers();
+            } else if (command.equals("add")) {
+                inputMembers();
+            } else if (command.equals("quit")) {
+                break;
+            } else {
+                System.out.println("유효하지 않는 명령입니다.");
+            }
+        }
+    }
+
+    private static String promptMenu() {
+        System.out.println("[메뉴]");
+        System.out.println("1.학생 관리");
+        System.out.println("2.강사 관리");
+        System.out.println("3.매니저 관리");
+        System.out.println("0.종료");
+        
+        while (true) {
+            System.out.print("메뉴 번호> ");
+            
+            String menu = keyIn.nextLine();
+            
+            switch (menu) {
+            case "1":
+            case "2":
+            case "3":
+            case "0":
+                return menu;
+            default:
+                System.out.println("메뉴 번호가 유효하지 않습니다.");
+            }
+        }
     }
     
     static void printMembers() {
