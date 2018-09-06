49873560dd07a57653b4297d8bd0b7cc9fcff9e0
eomjinyoung <jinyoung.eom@gmail.com>
2018-09-06 14:41:47 +0900

    LinkedList 생성

--- java110-project/src/main/java/bitcamp/java110/cms/util/LinkedList.java
+++ java110-project/src/main/java/bitcamp/java110/cms/util/LinkedList.java
@@ -32,11 +32,49 @@ public class LinkedList<T> {
     }
     
     public T remove(int index) {
-        return null;
+        if (index < 0 || index >= length) 
+            return null;
+        
+        length--;
+        
+        Node<T> cursor = first;
+        for (int count = 0; count < index; count++) {
+            cursor = cursor.next;
+        }
+        
+        if (cursor == first) {
+            first = first.next;
+            first.prev = null;
+            return cursor.value;
+        }
+         
+        cursor.prev.next = cursor.next;
+        cursor.next.prev = cursor.prev;
+        return cursor.value;
     }
     
     public void insert(int index, T obj) {
+        if (index < 0 || index >= length) 
+            return;
         
+        length++;
+        
+        Node<T> node = new Node<>();
+        node.value = obj;
+        
+        Node<T> cursor = first;
+        for (int count = 0; count < index; count++) {
+            cursor = cursor.next;
+        }
+        
+        // 이전 노드가 있어야만 이전 노드를 새 노드에 연결할 수 있다.
+        if (cursor != first) {
+            cursor.prev.next = node; // 이전 노드는 새 노드를 가리킨다.
+            node.prev = cursor.prev; // 새 노드는 이전 노드를 가리킨다.
+        }
+        
+        cursor.prev = node; // 현재 노드는 새 노드를 가리킨다.
+        node.next = cursor; // 새 노드의 다음은 현재 노드를 가리킨다.
     }
     
     public int size() {
