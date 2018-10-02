5757e3a91040b5b9be118920742da8963784308c
eomjinyoung <jinyoung.eom@gmail.com>
2018-10-02 12:16:59 +0900

    필터로 사용자 권한 검사하기

---
+++ java110-project/src/main/java/bitcamp/java110/cms/filter/AuthFilter.java
@@ -0,0 +1,56 @@
+package bitcamp.java110.cms.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import bitcamp.java110.cms.domain.Member;
+
+@WebFilter("/*")
+public class AuthFilter implements Filter {
+    @Override
+    public void doFilter(
+            ServletRequest request, 
+            ServletResponse response, 
+            FilterChain chain)
+            throws IOException, ServletException {
+        
+        HttpServletRequest httpRequest = (HttpServletRequest)request;
+        HttpServletResponse httpResponse = (HttpServletResponse)response;
+        
+        String servletPath = httpRequest.getServletPath();
+        
+        if (servletPath.endsWith("add") ||
+            servletPath.endsWith("delete")) {
+            
+            // 로그인 여부 검사
+            HttpSession session = httpRequest.getSession();
+            Member loginUser = (Member)session.getAttribute("loginUser");
+            if (loginUser == null) {
+                httpResponse.sendRedirect("/auth/login");
+                return;
+            }
+        }
+        
+        chain.doFilter(request, response);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
